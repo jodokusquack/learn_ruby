92217386e430498c1b96b2f12292a5e96d0e8aef
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-11-27 22:44:28 +0100

    Solve 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
 #write your code here
+def echo word
+  word
+end
+
+def shout word
+  word.upcase
+end
+
+def repeat(word, number = 2)
+  ([word]*number).join(" ")
+end
+
+def start_of_word(word, pos)
+  word[0..pos-1]
+end
+
+def first_word(sentence)
+  return sentence.scan(/\w+/)[0]
+end
+
+def titleize title
+   new_title = title.scan(/\w+/)
+   new_title.each_with_index do |word, index|
+    next if ["and", "the", "over"].include?(word) unless index == 0
+    new_title[index] = word.capitalize
+  end
+  return new_title.join(" ")
+
+end
