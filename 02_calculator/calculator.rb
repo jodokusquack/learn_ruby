072faf2b37015aded52b73b0858b44f9d8b51db6
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-11-27 22:13:20 +0100

    Solve 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+def add(a,b)
+  return a+b
+end
+
+def subtract(a,b)
+  return a-b
+end
+
+def sum(array)
+  sum = 0
+  array.each do |number|
+    sum += number
+  end
+  return sum
+end
+
+def multiply(*numbers)
+  return numbers.reduce(1, :*)
+end
+
+def power(a,b)
+  a**b
+end
+
+def factorial(a)
+  if a == 0
+    return 1
+  else 
+    return a * factorial(a-1)
+  end
+end
+
+
