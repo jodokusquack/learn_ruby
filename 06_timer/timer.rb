f1172eefc19ff69ce8ecd906244fd5c1fe796757
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-12-01 21:52:50 +0100

    Solve 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize seconds=0
+    @seconds = seconds
+  end
+
+  def seconds= seconds
+    @seconds = seconds
+  end
+
+  def time_string
+    seconds = @seconds % 60
+    minutes = @seconds.to_i / 60
+    hours = 0
+    while minutes > 60 do
+      hours += 1
+      minutes -= 60
+    end
+    return padded(hours) + ":" + padded(minutes) + ":" + padded(seconds)
+  end
+
+  def padded number
+    if number >= 10
+      return number.to_s
+    else
+      return "0" +  number.to_s
+    end
+  end
+
+
+  
 end
+
+a = Timer.new
+
+puts a.time_string
