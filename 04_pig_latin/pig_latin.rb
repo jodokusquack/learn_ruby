ac654b9534f5ddf0c94d2906d573b7da22e5b5f4
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-12-01 20:26:58 +0100

    Solve 04_pig_latin Part 1

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+
+
+def translate sentence
+  vowels = ["a", "e", "i", "o", "u"]
+  punctuation = [".", "?", "!"]
+
+  words = sentence.scan(/[\w\.!?]+/)
+  
+   new = words.map do |word|
+    if vowels.include?(word[0].downcase)
+      word.concat("ay")
+    elsif punctuation.include?(word[0])
+      word = word
+    else
+      first_vowel = word.index(/(?<![q])[aeiou]/)   # Negative lookbehind (meaning it looks behind and if it matches it doesn't count!). This only matches u's NOT preceded by a q
+      word[first_vowel..].concat(word[0..first_vowel-1],"ay")
+      
+    end
+  end
+  return new.join(" ")
+end
+
+print translate "I am the wolf."
