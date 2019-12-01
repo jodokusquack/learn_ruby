3a2c719be883741d0f087aad1a7dd7977ed7f45d
Jakob Blaschke <jakobblaschke@yahoo.de>
2019-12-01 21:37:00 +0100

    Solve 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,31 @@
 class Book
-# write your code here
+  @@test = 2
+
+  attr_accessor :title
+  
+  def title=(title)
+    cons_and_pres = ["and", "or", "but", "yet", "for", "nor", "so", "in", "the", "of", "a", "an"]
+    @test = 1
+    words = title.scan(/\w+/)
+    capitalized_words = words.map.with_index do |word, i|
+      if not cons_and_pres.include?(word)
+        word.capitalize
+      else
+        if i == 0
+          word.capitalize
+        else
+          word
+        end
+      end
+
+      
+    end
+     @title = capitalized_words.join(" ")
+  end
+
+  
 end
+
+a = Book.new
+a.title = "test"
+puts a.title
